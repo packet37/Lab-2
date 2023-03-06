317f344c55ba795d292a9d1a4f5f28169ab67e3a
packet37 <125910700+packet37@users.noreply.github.com>
2023-03-06 21:59:22 +0300

    Add files via upload

---
+++ Лабораторная_2_общая.cs
@@ -0,0 +1,20 @@
+﻿using Classes;
+using System;
+
+Console.WriteLine("Количество людей: ");
+int n = int.Parse(Console.ReadLine());
+Person[] Persons = new Person[n];
+
+for (int i = 0; i < n; i++)
+{
+    Persons[i] = new Person();
+    Persons[i].Read();
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(Persons[i].Surname);
+    Console.WriteLine(Persons[i].Name);
+    Console.WriteLine(Persons[i].Age(Persons[i].BirthDate));
+    Console.WriteLine(Persons[i].Gender);
+}
---
+++ Лабораторная_2_общая_класс.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set { if (value == 'M' | value == 'F' | value == 'm' | value == 'f') gender = value; }
+        }
+        public Person()
+        {
+
+        }
+        public override string ToString()
+        {
+            return Name + " " + Surname + " " + BirthDate + " " + gender;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {Name}  Фамилия: {Surname}  Возраст: {Age(BirthDate)} Пол: {gender}");
+        }
+        public void Read()
+        {
+            Console.WriteLine("Введите имя: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию: ");
+            Surname = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения: ");
+            BirthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите пол: ");
+            Gender = char.Parse(Console.ReadLine());
+        }
+        public int Age(DateTime birthDay)
+        {
+            int age = DateTime.Now.Year - birthDay.Year;
+
+            if (DateTime.Now.Day < birthDay.Day)
+            {
+                if (DateTime.Now.Month < birthDay.Month)
+                {
+                    age--;
+                }
+            }
+
+            return age;
+        }
+
+    }
+}
